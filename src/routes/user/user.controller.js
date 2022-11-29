85aa3fc4bdc8c14af1119d6830e92a8fae6f167c
Leonidas Ramirez <leonidas.ramirez.q@gmail.com>
2022-11-29 02:19:38 -0500

    adding data validation

--- src/routes/user/user.controller.js
+++ src/routes/user/user.controller.js
@@ -11,6 +11,18 @@ function httpGetUser(req, res) {
 
 function httpAddUser(req, res) {
     const newUser = req.body;
+
+    if(
+        !newUser.id ||
+        !newUser.name ||
+        !newUser.alter_ego ||
+        !newUser.phone ||
+        !newUser.email
+        ) {
+            return res.status(400).json({
+                error: "Missing required launch property"
+            });
+        }
     return res.status(201).json(addUser(newUser));
 }
 
